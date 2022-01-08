89178753055ca04e030149c7b3f6019d0487fd65
Maksym Pasichnyk <maximmkaaa@gmail.com>
2022-01-09 01:28:48 +0200

    remove unused

--- src/core/FrameInfo.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cstdlib>
-
-struct FrameInfo {
-	size_t index = 0;
-};
--- src/core/Shader.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <resources/ResourceLocation.hpp>
-
-struct Shader : Resource {
-	GLuint id;
-
-	explicit Shader(GLuint id) : id(id) {}
-};
